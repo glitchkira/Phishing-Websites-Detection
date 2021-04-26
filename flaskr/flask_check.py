082fb2bbc99e2dd87db63e644873a81aeb5cf0d4
akira <928943846@qq.com>
2021-04-26 22:02:20 +0800

    add extension sql

--- flaskr/flask_check.py
+++ flaskr/flask_check.py
@@ -1,10 +1,37 @@
 from .myPredModel import myModel
+from flaskr.phishTankApiRequest import PhishTank
 # import os
 
 # import sys
 import time
 
 # currentDir = os.path.dirname(__file__)
+def checkWithSql(url):
+    """ Check the user input url with sql
+    """
+    urlInSql = 0
+    time_start= time.time()
+    # return 0 # overlook this func
+    phishTank = PhishTank()
+    result = phishTank.check(url)
+    if result.in_database:
+        # url exists in SQL
+        urlInSql = 1
+        if result.valid:
+            print("{url} is a phish!".format(url=result.url))
+            # flash("Phishing Website: \n" + url, 'danger')
+        else:
+            print("{url} is not a phish!".format(url=result.url))
+            # flash("Legitimate Website: \n" + url, 'success')
+    else:
+        # url Not exists in SQL
+        urlInSql = 0
+        print("{url} is not in the PhishTank database".format(url=result.url))
+    time_end = time.time()
+    time_c = time_end - time_start 
+    print('time cost for sql', time_c, 's')
+
+    return urlInSql
 
 def check(url):
     ''' Make prediction with trained model
@@ -15,10 +42,17 @@ def check(url):
     if 'http' not in url:
         url = 'https://www.' + url
     print("url to predict is:" + url)
+    # Check with sql first
+    try:
+        urlInSql = checkWithSql(url)
+    except Exception as error:
+        print("Sql failed: %s" % error)
 
     try:
-        model = myModel()
-        judge = model.pipeline(url)[0]
+        if not urlInSql:
+            # if url not exist in sql, then check with machine learning model
+            model = myModel()
+            judge = model.pipeline(url)[0]
     except FileNotFoundError:
         judge = 2
         print('Exception: model Not Found')
